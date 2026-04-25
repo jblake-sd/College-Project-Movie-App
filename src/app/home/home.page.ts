39c6dfb7156ddddf2c0a87d63c96e2a9753b1caa
unknown <jessica.blake.92@gmail.com>
2026-04-25 21:23:02 +0000

    Get and display trending movie titles from API

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [ IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NgFor],
 })
 
 export class HomePage {
+
+  movies: any[] = [];
+
   constructor(private http: HttpClient) {
     const apiKey = '16116c99d8cfec890d546c27498eadb6';
-    this.http.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`).subscribe((data) => {console.log(data);
+    this.http.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`).subscribe((data: any) => {this.movies = data.results; console.log(this.movies);
 
     })
   }
+
 }
