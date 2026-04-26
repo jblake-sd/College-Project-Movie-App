6e7454d1f820f51807f31b65e6011514c89b697f
unknown <jessica.blake.92@gmail.com>
2026-04-26 11:24:28 +0000

    Added extra features on Trending Movies page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardHeader, IonCardContent } from '@ionic/angular/standalone';
+import { IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardHeader, IonCardContent } from '@ionic/angular/standalone';
 import { HttpClient } from '@angular/common/http';
 import { NgFor } from '@angular/common'
+import { DecimalPipe } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NgFor, IonCard, IonCardTitle, IonCardHeader, IonCardContent],
+  imports: [IonButton, RouterLink, DecimalPipe, IonHeader, IonToolbar, IonTitle, IonContent, NgFor, IonCard, IonCardTitle, IonCardHeader, IonCardContent],
 })
 
 export class HomePage {
 
   movies: any[] = [];
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
     const apiKey = '16116c99d8cfec890d546c27498eadb6';
     this.http.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`).subscribe((data: any) => {this.movies = data.results; console.log(this.movies);
 
