9aadfcc9ff3b9bd408a6e547a7f474bd564bf9e0
unknown <jessica.blake.92@gmail.com>
2026-04-26 16:18:42 +0000

    Search bar added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,26 +1,37 @@
 import { Component } from '@angular/core';
-import { IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardHeader, IonCardContent } from '@ionic/angular/standalone';
+import { IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardHeader, IonCardContent, IonSearchbar } from '@ionic/angular/standalone';
 import { HttpClient } from '@angular/common/http';
 import { NgFor } from '@angular/common'
 import { DecimalPipe } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonButton, RouterLink, DecimalPipe, IonHeader, IonToolbar, IonTitle, IonContent, NgFor, IonCard, IonCardTitle, IonCardHeader, IonCardContent],
+  imports: [IonButton, DecimalPipe, IonHeader, IonToolbar, IonTitle, IonContent, NgFor, IonCard, IonCardTitle, IonCardHeader, IonCardContent, IonSearchbar],
 })
 
 export class HomePage {
+
+  pageTitle: string = "Today's Trending Movies";
 
   movies: any[] = [];
 
   constructor(private http: HttpClient, private router: Router) {
     const apiKey = '16116c99d8cfec890d546c27498eadb6';
     this.http.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`).subscribe((data: any) => {this.movies = data.results; console.log(this.movies);
+        })
+  }
 
+  onSearch(event: any) {
+    const query = event.target.value;
+
+    this.pageTitle = "Search Results";
+    
+    const apiKey = '16116c99d8cfec890d546c27498eadb6';
+    this.http.get(`https://api.themoviedb.org/3/search/movie?&api_key=${apiKey}&query=${query}`).subscribe((data: any) => {this.movies = data.results;
     })
   }
 
